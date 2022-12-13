18693df07d6f1b716960f1bd618bed3810855f00
maxwellmuchiri <muchirimaxwel@gmail.com>
2022-12-13 14:06:23 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide(){
+    let divide = 2000/100
+    return divide;
+}
+
+function square(){
+    let square = 2*2
+    return square;
+}
+
+function add(a ,b ){
+    let add = (a + b);
+ return add;
+}
